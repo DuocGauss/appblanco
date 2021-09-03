1af0e26d297a21ff64ac96a9b0ce9a7ea806e281
Gustavo <gu.martinezb@duocuc.cl>
2021-09-03 18:57:26 -0400

    Resguardo app de entrenamiento

---
+++ src/app/pages/pag2/pag2.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pag2',
+  templateUrl: './pag2.page.html',
+  styleUrls: ['./pag2.page.scss'],
+})
+export class Pag2Page implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
