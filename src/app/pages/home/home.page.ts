57c803cb9e6b491b0fe5688f869afc37429b1413
Gustavo <gu.martinezb@duocuc.cl>
2021-10-29 21:13:13 -0300

    codigoqr

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 
 
@@ -13,7 +14,7 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
   nombreusuario='';
 
-  constructor(private activeroute:ActivatedRoute, private router:Router, private alertController:AlertController) {
+  constructor(private activeroute:ActivatedRoute, private router:Router, private alertController:AlertController, private qr:QRScanner) {
     this.activeroute.queryParams.subscribe(params => {
 
       if (this.router.getCurrentNavigation().extras.state) {
@@ -26,7 +27,7 @@ export class HomePage {
  
    });
   }
-
+  
 
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -41,7 +42,35 @@ export class HomePage {
     console.log('onDidDismiss resolved with role', role);
   }
 
+ngOnInit(){
+  this.StartScanning();
+}
+
+  StartScanning(){
+
   
-  
+// Optionally request the permission early
+this.qr.prepare()
+.then((status: QRScannerStatus) => {
+   if (status.authorized) {
+     // camera permission was granted
+     this.qr.show();
+     document.getElementsByTagName("body")[0].style.opacity = "0.5";
+     this.qr.scan().subscribe((val)=>{
+       alert(val);
+       document.getElementsByTagName("body")[0].style.opacity = "1";
+     })    
+
+   } else if (status.denied) {
+     // camera permission was permanently denied
+     // you must use QRScanner.openSettings() method to guide the user to the settings page
+     // then they can grant the permission from there
+   } else {
+     // permission was denied, but not permanently. You can ask for permission again at a later time.
+   }
+})
+.catch((e: any) => console.log('Error is', e));
+
+} 
   
 }
