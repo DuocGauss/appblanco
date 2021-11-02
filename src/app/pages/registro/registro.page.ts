087471adf7069aa68b781cdb58a52c0cce94404c
Gustavo <gu.martinezb@duocuc.cl>
2021-11-02 17:49:09 -0300

    qr

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { Storage } from '@ionic/storage-angular'
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -15,7 +16,7 @@ export class RegistroPage implements OnInit {
     password:'',
     estado:0
   }
-  constructor(private storage:Storage) { }
+  constructor(private storage:Storage, private alertController:AlertController, private navCtrl:NavController) { }
 
   ngOnInit() {
   }
@@ -29,6 +30,29 @@ export class RegistroPage implements OnInit {
   async guardar(user:Usuario)
   {
     await this.storage.set(user.username,user);
+  }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      
+      header: 'Correcto',
+      message: 'Cuenta creada',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+          handler: () => {
+            this.navCtrl.navigateRoot('/login');
+          }
+        }
+      ]
+    }); 
+
+    await alert.present();
+
+    const { role } = await alert.onWillDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
